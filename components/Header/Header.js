9f80c0617b7aa5ac6003eb10145397b7c1151f43
patcharaleelalumplert@gmail.com <patcharaleelalumplert@gmail.com>
2017-06-11 18:16:36 +0700

    mock data front

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 const Header = () => (
   <Head>
     <title>MileTrav Everything</title>
-    <link rel="stylesheet" href="/asset/bootstrap.css" />
-    <link rel="stylesheet" href="/asset/miletrav.css" />
+    <link rel="stylesheet" href="/asset/css/bootstrap.css" />
+    <link rel="stylesheet" href="/asset/css/miletrav.css" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
 )
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,38 @@ export default () => (
   <div>
     <Header />
     <Navbar />
-    <div className="container section" style={{ height: 1500 }}>
+    <div className="section">
       Hello world
+    </div>
+    <div className="quote-info">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3 col-sm-12">
+            <h1 className="text-center">
+                Where ever you go is apart of you somehow
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
     <style jsx global>
       {`
-            .section {
-              margin-top: 80px;
-            }
+          .section {
+               width: 100%;
+               /*background: url('/asset/img/bg001.jpg') center center no-repeat;
+               background-size: cover;*/
+               position: relative;
+               height: 550px;
+          }
+          .quote-info {
+               width: 100%;
+               position: relative;               
+               padding: 50px;
+               background: #ce0b15;               
+          }
+          .quote {
+
+          }
       `}
     </style>
   </div>
